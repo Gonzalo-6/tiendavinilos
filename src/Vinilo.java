e9b3eabf8d0ddb9f62e4e017ed6a5afa59b72c15
Gonzalo-6 <g-arranz@hotmail.com>
2025-07-29 09:04:51 +0200

    Introducción vinilos

--- src/Vinilo.java
+++ src/Vinilo.java
@@ -1,4 +1,29 @@
 
+import java.io.Serializable;
+public class Vinilo implements Serializable {
+    private String titulo;
+    private String artista;
+    private double precio;
+    private int stock;
+
+
+    public Vinilo(String titulo, String artista, double precio, int stock) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getTitulo() { return titulo; }
+    public double getPrecio() { return precio; }
+    public int getStock() { return stock; }
+    public void reducirStock() { if (stock > 0) stock--; }
+
+    @Override
+    public String toString() {
+        return "\"" + titulo + "\" de " + artista + " - " + precio + "€ - Stock: " + stock;
+    }
+}
 
 
 
